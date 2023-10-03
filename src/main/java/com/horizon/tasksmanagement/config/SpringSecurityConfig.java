825088c5efff445a74e5bfb16e135d59c7c90b7c
TheLordZedd <pcgpod@yahoo.com>
2023-10-02 23:59:13 -0500

    feat:
    - More security configuration
    - Updating database authentication to spring 6 (incomplete)

--- src/main/java/com/horizon/tasksmanagement/config/SpringSecurityConfig.java
+++ src/main/java/com/horizon/tasksmanagement/config/SpringSecurityConfig.java
@@ -1,21 +1,25 @@
 package com.horizon.tasksmanagement.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SpringSecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -38,20 +42,27 @@ public class SpringSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-
-        UserDetails frankgrimes = User.builder()
-                .username("frankgrimes")
-                .password(passwordEncoder().encode("password123"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(frankgrimes, admin);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails frankgrimes = User.builder()
+//                .username("frankgrimes")
+//                .password(passwordEncoder().encode("password123"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(frankgrimes, admin);
+//    }
+
+
 }
